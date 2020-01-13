3e3d8f2516027a4412dac1e7ad1eb39768c48796
Christian R. Vozar <christian@rogueethic.com>
2020-01-13 16:05:03 -0700

    go: http client helper

---
+++ client.go
@@ -0,0 +1,18 @@
+// Package rootca provides a http.Client with a certificate bundle.
+package gorootca
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"net/http"
+)
+
+// New returns a http.Client with a pre-configured root certificate bundle.
+func New() *http.Client {
+	cp := x509.NewCertPool()
+	cp.AppendCertsFromPEM(rootCertificateBundle)
+
+	return &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{RootCAs: cp}}}
+}
